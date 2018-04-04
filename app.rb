898c70d732034d7293272673c5a23f0342be0215
harim1206 <harim1206@gmail.com>
2018-04-04 13:12:53 -0400

    progress

--- app.rb
+++ app.rb
@@ -5,5 +5,39 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+
+      
+      erb :index
+    end
+
+    get '/pirates' do
+      @pirates = Pirate.all
+      @ships = Ship.all
+
+      erb :show
+    end
+
+    get '/pirates/new' do
+
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+
+      params[:pirate][:ships].each do |ship_hash|
+        @ship = Ship.new(ship_hash)
+
+      end
+
+      redirect '/pirates'
+    end
+
+
+
+
+
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,41 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+  @@all = []
+
+  def initialize (name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+
+  end
+
+
 end
+
+# params = {
+#   :pirate => {
+#     name: "jack sparrow",
+#     weight: "230",
+#     height: "80 inches"
+#     :ships =>[
+#       {
+#         name: "flying dutchmen",
+#         type: "big one",
+#         booty: "gold"
+#
+#       },
+#       {
+#         name: "pearl",
+#         type: "small one",
+#         booty: "rum"
+#       }
+#     ]#/ships array
+#   }#/:pirate
+# }
